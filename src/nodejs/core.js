29d379012a9a46fb5520b42b5c5c3d4607208ef2
aong <yxwzaxns@gmail.com>
2020-04-22 10:46:29 +0800

    add test

--- src/nodejs/core.js
+++ src/nodejs/core.js
@@ -5,7 +5,11 @@ const log = (...i) => {
 exports.log = log
 
 const removeDuplicate = (arr, key = 'id') => {
-    return arr.filter((item, index, self) => self.findIndex(t => t[key] === item[key]) === index)
+    if(JSON.stringify(arr[0])[0]==='{'){
+        return arr.filter((item, index, self) => self.findIndex(t => t[key] === item[key]) === index)
+    }else{
+        return [...new Set(arr)]
+    }
 }
 exports.removeDuplicate = removeDuplicate
 
@@ -27,19 +31,20 @@ const loopTask = async (data, func, options) => {
         fps: 100
     }, cliProgress.Presets.shades_classic);
     infoBar.start(data.length, 1)
-    for (let i = 0; i < data.length; i += options.execLength) {
-        const subData = data.slice(i, i + options.execLength)
+    for (let i = 0; i < data.length; i += +options.execLength) {
+        const subData = data.slice(i, i + +options.execLength)
         const taskMap = subData.map(e => {
             return (async () => {
-                return await func(e)
+                return func(e)
             })()
         })
-        for (let task of taskMap) {
-            const res = await task
+        for (const j in taskMap) {
+            const res = await taskMap[j]
+            infoBar.update(i + +j + 1)
             results.push(res)
         }
         await sleep(options.timeGap)
-        infoBar.update(i + options.execLength)
+        
     }
     infoBar.stop()
     return results
--- test/nodejs/core.test.js
+++ test/nodejs/core.test.js
@@ -1,11 +1,44 @@
-const assert = require('assert')
+const assert = require('assert').strict
 const core = require('../../src/nodejs/core')
 
-describe('#loopTask()', function () {
-    it('loopTask([1,2,3] return', async () => {
-        const ret = await core.loopTask([1,2,3],(e)=>{
-            return e+1
+describe('#coreTest', function () {
+    describe('#loopTask()', function () {
+        it('loopTask([1,2,3]) return [2, 3, 4]', async () => {
+            const ret = await core.loopTask([1, 2, 3], (e) => {
+                return e + 1
+            })
+            assert.deepEqual(ret, [2, 3, 4])
         })
-        assert.deepEqual(ret, [2, 3, 4])
+        it('loopTask([1,2,3]) return [2, 3, 4] immediately', async () => {
+            const ret = await core.loopTask([1, 2, 3], (e) => {
+                return e + 1
+            }, { timeGap: 0, execLength: 2 })
+            assert.deepEqual(ret, [2, 3, 4])
+        })
+    })
+    describe('#range()', function () {
+        const p = [0,4]
+        const res = [0, 1, 2, 3, 4]
+        it(`range(0,4) return ${res}`, async () => {
+            const ret = await core.range(...p)
+            assert.deepEqual(ret, res)
+        })
+    })
+    describe('#removeDuplicate()', function () {
+        const p = [0,1,2,2,3,4,4]
+        const res = [0, 1, 2, 3, 4]
+        it(`removeDuplicate(${p}) return ${res}`, async () => {
+            const ret = await core.removeDuplicate(p)
+            assert.deepEqual(ret, res)
+        })
+    })
+    describe('#removeDuplicate()', function () {
+        const p = [{ id: 1, name: 'a' }, { id: 2, name: 'b' }, { id: 1, name: 'a' }]
+        const res = [{ id: 1, name: 'a' }, { id: 2, name: 'b' }]
+        it(`removeDuplicate(${p}) return ${res}`, async () => {
+            const ret = await core.removeDuplicate(p)
+            assert.deepEqual(ret, res)
+        })
     })
 })
+
