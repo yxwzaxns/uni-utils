ad6d82b7d69aa6caf07b87d3475a29d749b1e179
aong <yxwzaxns@gmail.com>
2020-06-23 18:29:33 +0800

    add process helper

--- src/nodejs/process.js
+++ src/nodejs/process.js
@@ -1,9 +1,20 @@
 const restartProcess = () => {
     const subp = require('child_process').spawn(process.argv[0], process.argv.splice(1), {
         detached: true,
-        stdio: ['ignore', 'inherit', 'inherit']
+        stdio: ['ignore', 'ignore', 'ignore']
     })
     subp.unref()
     process.exit()
 }
 exports.restartProcess = restartProcess
+
+const createProcess = (file, args) => {
+    const w = require('child_process').fork(file, args, {
+        detached: true,
+        stdio: ["ignore", "ignore", "ignore", 'ipc']
+    })
+    w.unref()
+    w.channel.unref()
+    return w
+}
+exports.createProcess = createProcess
